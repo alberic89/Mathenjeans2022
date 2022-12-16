723ab576c06509dbc24fcf3ed557c6c158323e35
alberic89 <alberic89@gmx.com>
2022-12-16 19:27:05 +0100

    Create main.py

---
+++ Marienbad/main.py
@@ -0,0 +1,25 @@
+#!/bin/env/python3
+
+from itertools import combinations_with_replacement
+
+def main():
+	number = list(range(1,10))
+	for row in range(1,10):
+		temp = combinations_with_replacement(number, row)
+		for i in list(temp):
+			if isGood(i) :
+				print(i)
+
+def isGood(combinations):
+	somme = 0
+	for j in combinations :
+		somme += int(bin(j)[2:])
+	good = True
+	for k in str(somme):
+		if int(k)%2!=0 :
+			good = False
+			break
+	return good
+
+if __name__ == "__main__" :
+	main()
