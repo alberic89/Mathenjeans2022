674afcf1f82c99413ad9bb8561d19e6043a8d43a
alberic89 <alberic89@gmx.com>
2022-12-19 12:52:30 +0100

    Update main.py

--- Marienbad/main.py
+++ Marienbad/main.py
@@ -2,23 +2,30 @@
 
 from itertools import combinations_with_replacement
 
-def main():
-	number = list(range(1,10))
-	for row in range(1,10):
+def generate(nbjetons=10,nbtas=10):
+	number = list(range(1,nbjetons+1))
+	for row in range(1,nbtas+1):
 		temp = combinations_with_replacement(number, row)
 		for i in list(temp):
 			if isGood(i) : print(i)
 
-def isGood(combinations):
-	somme = 0
-	for j in combinations :
-		somme += int(bin(j)[2:])
-	good = True
-	for k in str(somme):
-		if int(k)%2!=0 :
-			good = False
+
+def isGood(liste):
+	ok = True
+	liste = list(liste)
+	maxlen = len(bin(max(liste))[2:])
+	for i in range(len(liste)):
+		liste[i]=bin(liste[i])[2:]
+	l = tuple(liste)
+	for i in range(1,maxlen+1):
+		s = 0
+		for obj in l :
+			if i <= len(obj) :
+				s += int(obj[-i])
+		if s%2 != 0:
+			ok = False
 			break
-	return good
+	return ok
 
 if __name__ == "__main__" :
-	main()
+	generate()
